7d175a5a3155318c7e40f21519ec1c7f3e8ccd34
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-12 14:05:58 +0100

    create link grahpql mutation implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
-import LinkList from './LinkList'
+import React, { Component } from "react";
+import "../styles/App.css";
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return <LinkList />;
   }
 }
 
