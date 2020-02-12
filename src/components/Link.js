d844b2257ce38dfd9f5b27857da3f88f88b62c2c
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-12 16:23:56 +0100

    vote mutation added for link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react'
-import { AUTH_TOKEN } from '../constants'
-import { timeDifferenceForDate } from '../utils'
+import React, { Component } from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 class Link extends Component {
   render() {
-    const authToken = localStorage.getItem(AUTH_TOKEN)
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
       <div className="flex mt2 items-start">
         <div className="flex items-center">
           <span className="gray">{this.props.index + 1}.</span>
           {authToken && (
-            <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
-              ▲
-            </div>
+            <Mutation
+              mutation={VOTE_MUTATION}
+              variables={{ linkId: this.props.link.id }}
+            >
+              {voteMutation => (
+                <div className="ml1 gray f11" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
           )}
         </div>
         <div className="ml1">
@@ -20,16 +49,16 @@ class Link extends Component {
             {this.props.link.description} ({this.props.link.url})
           </div>
           <div className="f6 lh-copy gray">
-            {this.props.link.votes.length} votes | by{' '}
+            {this.props.link.votes.length} votes | by{" "}
             {this.props.link.postedBy
               ? this.props.link.postedBy.name
-              : 'Unknown'}{' '}
+              : "Unknown"}{" "}
             {timeDifferenceForDate(this.props.link.createdAt)}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Link
+export default Link;
