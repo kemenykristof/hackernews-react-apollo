97b4cabe2101ea6fd2e211a99cd39a8b9e2dfe98
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-12 17:23:19 +0100

    websocketlink setup

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
 
@@ -28,11 +31,30 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
-})
+});
 
 // 4
 ReactDOM.render(
