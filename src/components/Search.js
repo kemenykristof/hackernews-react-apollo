7b49fe9d3210a5e41b36df217ea8645544ff3755
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-12 17:09:51 +0100

    search implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react'
-import { withApollo } from 'react-apollo'
-import gql from 'graphql-tag'
-import Link from './Link'
+import React, { Component } from "react";
+import { withApollo } from "react-apollo";
+import gql from "graphql-tag";
+import Link from "./Link";
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
 
 class Search extends Component {
-
   state = {
     links: [],
-    filter: ''
-  }
+    filter: ""
+  };
 
   render() {
     return (
@@ -16,7 +38,7 @@ class Search extends Component {
         <div>
           Search
           <input
-            type='text'
+            type="text"
             onChange={e => this.setState({ filter: e.target.value })}
           />
           <button onClick={() => this._executeSearch()}>OK</button>
@@ -25,12 +47,18 @@ class Search extends Component {
           <Link key={link.id} link={link} index={index} />
         ))}
       </div>
-    )
+    );
   }
 
   _executeSearch = async () => {
-    // ... you'll implement this 🔜
-  }
+    const { filter } = this.state;
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter }
+    });
+    const links = result.data.feed.links;
+    this.setState({ links });
+  };
 }
 
-export default withApollo(Search)
+export default withApollo(Search);
